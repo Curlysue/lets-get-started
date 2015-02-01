ac5c3f9a0a2041c2714574d8ec2e05a3796fee31
Curlysue <kirsty.portman@hotmail.com>
2015-02-01 21:44:23 +0000

    Update main.c

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 
 int main()
 {
-char answer = 0;
+   char answer = 0;
 
-printf("Enter any charictar. ");
-scanf("%c",&answer);
-getchar();//kill the return
-system("clear");
-printf("You have entered 0x%02x in hex\n",answer);
-printf("you have entered %o in octal\n", answer);
-printf("you have entered %i in decimal\n", answer);
+   printf("Enter any charictar. ");
+   scanf("%c",&answer);
+   getchar();//kill the return
+   system("clear");
+   printf("You have entered 0x%02x in hex\n",answer);
+   printf("you have entered %o in octal\n", answer);
+   printf("you have entered %i in decimal\n", answer);
 return 0;
 }
