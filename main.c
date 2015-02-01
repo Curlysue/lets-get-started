98a7283a28c5e9d1334128cbcc7ec4e81929ecec
Curlysue <kirsty.portman@hotmail.com>
2015-02-01 21:41:31 +0000

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+char answer = 0;
+
+printf("Enter any charictar. ");
+scanf("%c",&answer);
+getchar();//kill the return
+system("clear");
+printf("You have entered 0x%02x in hex\n",answer);
+printf("you have entered %o in octal\n", answer);
+printf("you have entered %i in decimal\n", answer);
+return 0;
+}
